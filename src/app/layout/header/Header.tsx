0b387fa7d948fb66003d3c80755dcbe932bd1a64
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-01-30 21:21:10 +0700

    Add header layout

---
+++ src/app/layout/header/Header.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import React from 'react'
+import './header.scss'
+type Props = {}
+
+export default function Header({}: Props) {
+	return (
+		<header id='head'>
+			<h1 style={{display:'none'}}>Winnie The Blue</h1>
+			<figure>
+				<div className="decor_back"></div>
+				<img src="/images/graphics/main-logo.png" alt="" className='main-logo' />
+			</figure>
+			<nav id='navigation'>
+				<Link href={'/'} className='nav-link active'>HOME</Link>
+				<Link href={'/service/credentials'} className='nav-link'>SERVICE</Link>
+				<Link href={'/terms'} className='nav-link'>TERMS</Link>
+				<Link href={'/clients'} className='nav-link'>CLIENTS</Link>
+			</nav>
+		</header>
+	)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Header from "./layout/header/Header";
 
 export default function Home() {
   return (
