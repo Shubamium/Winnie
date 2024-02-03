8a4648a030639437132674adcb37bcdb17631612
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-02-03 07:41:39 +0700

    Fix some minor issue

--- src/app/layout/header/Header.tsx
+++ src/app/layout/header/Header.tsx
@@ -1,14 +1,19 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
 import './header.scss'
 import Navigation from './navigation/Navigation'
+import { useRouter} from 'next/navigation';
 type Props = {}
 
 export default function Header({}: Props) {
+	const navigate = useRouter();
 	return (
 		<header id='head'>
 			<h1 style={{display:'none'}}>Winnie The Blue</h1>
-			<figure>
+			<figure onClick={()=>{
+				navigate.replace('/');
+			}}>
 				<div className="decor_back"></div>
 				<img src="/images/graphics/main-logo.png" alt="" className='main-logo' />
 			</figure>
